235a65837fca0077988231449cff0c0d4a90024b
wilsonibekason <wanbekgithub@gmail.com>
2022-04-09 15:09:35 -0700

    completed and have added header section

--- front-end/src/containers/About/About.jsx
+++ front-end/src/containers/About/About.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { useState, useEffect } from 'react';
+import { images } from '../../constants';
 import './About.scss'
 
 const About = () => {
+  const abouts = [
+    {title: 'web development', description: 'i am a web developer', imgUrl: images.about02},
+    {title: 'web development', description: 'i am a web developer', imgUrl: images.about01},
+    {title: 'web development', description: 'i am a web developer', imgUrl: images.about03},
+    {title: 'web development', description: 'i am a web developer', imgUrl: images.about04}
+  ]
   return (
-    <div>About</div>
+    <>
+    <h2 className='head-text'>I know That <span>Good Designs</span>
+    <br />
+     means{'  '}
+    <span>Good Business</span>
+     </h2>
+     <div className="app__profiles">
+          {abouts.map((about, index) => (
+            <motion.div 
+          whileInView={{opacity: 1}}
+          whileHover={{scale: 1.1}}
+          transition={{duration: .5, type: 'twen'}}
+          className='app__profile-item'
+          key={about.title + index}
+            >
+           <img src={about.imgUrl} alt={about.title} />
+           <h2 className='bold-text' style={{marginTop: 20}}>
+             {about.title}
+           </h2>
+           <p className='bold-text' style={{marginTop: 10}}>
+             {about.description}
+           </p>
+            </motion.div>
+          ))}
+     </div>
+    </>
   )
 }
 
